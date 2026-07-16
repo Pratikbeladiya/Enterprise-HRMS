44d9966fa310189dfb2f536a7761b897fc26ff44
Pratik Beladiya <beladiyapratik03@gmail.com>
2026-07-16 12:35:14 +0530

    feat: configure helmet and CORS for backend security (refs #10)

--- src/server/app.js
+++ src/server/app.js
@@ -1,13 +1,26 @@
 const express = require("express");
-const app = express();//server create successfully
+const helmet = require("helmet");
+const cors = require("cors");
+
+const app = express();
+
 const authRoute = require("./routes/user.route");
 
+// Body Parsing Middleware
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/user",authRoute);
+// Security Middleware
+app.use(helmet());
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
+// Routes
+app.use("/api/user", authRoute);
 
-module.exports=app;
+module.exports = app;
