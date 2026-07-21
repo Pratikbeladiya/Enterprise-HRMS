29926ac6d9918ea1ab26e068972a0041a93160bf
utsavbeladiya268-dot <utsavbeladiya268@gmail.com>
2026-07-21 10:20:54 +0530

    bring my frontend code back in this branch

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Home from "./pages/Home";
+import Attendance from "./pages/Attendance";
+import Payroll from "./pages/Payroll";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+
+        <Route path="/" element={<Login />} />
+
+        <Route path="/signup" element={<Signup />} />
+
+        <Route path="/home" element={<Home />} />
+
+        <Route
+          path="/attendance"
+          element={<Attendance />}
+        />
+
+        <Route
+          path="/payroll"
+          element={<Payroll />}
+        />
+
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
