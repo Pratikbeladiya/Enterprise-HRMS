2256ed372e597c1bce357faf8772014909954ecd
Arka <arkamondal1298@gmail.com>
2026-07-21 21:56:47 +0530

    dark mode effect from App component

--- HRMSfrontend/src/App.jsx
+++ HRMSfrontend/src/App.jsx
@@ -18,13 +18,7 @@ export default function App() {
   const [isCollapsed, setIsCollapsed] = useState(false); 
 
   // Handle Dark Mode globally
-  useEffect(() => {
-    if (darkMode) {
-      document.documentElement.classList.add('dark');
-    } else {
-      document.documentElement.classList.remove('dark');
-    }
-  }, [darkMode]);
+  
 
   const toggleDarkMode = () => setDarkMode(!darkMode);
 
