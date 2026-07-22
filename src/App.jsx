7d6cca028ada54906e77f468bf31d74b9e43e6bd
utsavbeladiya268-dot <utsavbeladiya268@gmail.com>
2026-07-22 12:31:29 +0530

    Update App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import Home from "./pages/Home";
+import Home from "./pages/Dashboard";
 import Attendance from "./pages/Attendance";
 import Payroll from "./pages/Payroll";
 
@@ -15,7 +15,7 @@ function App() {
 
         <Route path="/signup" element={<Signup />} />
 
-        <Route path="/home" element={<Home />} />
+        <Route path="/home" element={<Dashboard />} />
 
         <Route
           path="/attendance"
