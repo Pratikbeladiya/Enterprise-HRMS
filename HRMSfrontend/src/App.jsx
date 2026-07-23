26e989de74bdfdd10330a7dda33a0cd545d0b8d4
Arka <arkamondal1298@gmail.com>
2026-07-23 23:09:53 +0530

    Add Attendence module

--- HRMSfrontend/src/App.jsx
+++ HRMSfrontend/src/App.jsx
@@ -15,6 +15,9 @@ export default function App() {
   const [darkMode, setDarkMode] = useState(false);
   const [activeTab, setActiveTab] = useState('Dashboard');
   const [sidebarOpen, setSidebarOpen] = useState(false); 
+  const [isColl
+
+  
   const [isCollapsed, setIsCollapsed] = useState(false); 
 
   // Handle Dark Mode globally
