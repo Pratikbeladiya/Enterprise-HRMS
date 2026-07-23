685bf8c51cbb0fb362fd23da138be66318cbd4c6
utsavbeladiya268-dot <utsavbeladiya268@gmail.com>
2026-07-23 10:48:56 +0530

    Update App routes of Leave page

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,24 @@ import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
 import Attendance from "./pages/Attendance";
 import Payroll from "./pages/Payroll";
+import Leave from "./pages/Leave";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
 
+        {/* Authentication */}
         <Route path="/" element={<Login />} />
-
         <Route path="/signup" element={<Signup />} />
 
+        {/* Dashboard */}
         <Route path="/dashboard" element={<Dashboard />} />
 
-        <Route
-          path="/attendance"
-          element={<Attendance />}
-        />
-
-        <Route
-          path="/payroll"
-          element={<Payroll />}
-        />
-
-       
+        {/* HRMS Pages */}
+        <Route path="/attendance" element={<Attendance />} />
+        <Route path="/payroll" element={<Payroll />} />
+        <Route path="/leave" element={<Leave />} />
 
       </Routes>
     </BrowserRouter>
