cacfef501b121e254a2cb15c1c66cb8f319a07dd
Pratik Beladiya <beladiyapratik03@gmail.com>
2026-07-24 11:53:21 +0530

    add the frontend main file

--- src/server/server.js
+++ src/server/server.js
@@ -10,5 +10,5 @@ ConnectToDb();
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`App is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
