6faafd3a3c956550654958b0be733b501f89e578
Pratik Beladiya <beladiyapratik03@gmail.com>
2026-07-25 19:25:07 +0530

    improve code readability of department and employee route

--- src/server/routes/department.route.js
+++ src/server/routes/department.route.js
@@ -10,10 +10,19 @@ const {
   deleteDepartment
 } = require("../controllers/department.controller");
 
+//create a new department
 router.post("/", createDepartment);
+
+//get all departments
 router.get("/", getAllDepartments);
+
+//get department by id 
 router.get("/:id", getDepartmentById);
+
+//update department by id 
 router.put("/:id", updateDepartment);
+
+//delete department by id 
 router.delete("/:id", deleteDepartment);
 
 module.exports = router;
--- src/server/routes/employee.route.js
+++ src/server/routes/employee.route.js
@@ -9,10 +9,20 @@ const {
   deleteEmployee
 } = require("../controllers/employee.controller");
 
+
+//get all employees
 router.get("/", getAllEmployees);
+
+//create a new employee
 router.post("/", createEmployee);
+
+//get employee by id 
 router.get("/:id", getEmployeeById);
+
+//update employee by id
 router.put("/:id", updateEmployee);
+
+//delete employee by id 
 router.delete("/:id", deleteEmployee);
 
 module.exports = router;
