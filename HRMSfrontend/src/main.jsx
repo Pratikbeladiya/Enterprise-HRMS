7bb4ad0d7fe245b347339ab578a05e11cb0a898d
Arka <arkamondal1298@gmail.com>
2026-07-27 19:56:53 +0530

    Update Main.jsx

--- HRMSfrontend/src/main.jsx
+++ HRMSfrontend/src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import App from './App.jsx'
 
 // Replace with your actual Google Client ID from the Google Cloud Console
-const GOOGLE_CLIENT_ID = "Apna code daal do bhaiii.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID = "Apna code daal do bhaiii pleaseee mere pas paisa nahi hain.apps.googleusercontent.com";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
