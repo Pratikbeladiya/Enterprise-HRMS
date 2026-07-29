401a7e497cae08a6d11a7df2a7da6d09b7fb7725
Pratik Beladiya <beladiyapratik03@gmail.com>
2026-07-29 17:52:23 +0530

    add the employee add template page

---
+++ src/client/src/pages/employees/AddEmployee.jsx
@@ -0,0 +1,11 @@
+function AddEmployee() {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold">
+        Add Employee
+      </h1>
+    </div>
+  );
+}
+
+export default AddEmployee;
