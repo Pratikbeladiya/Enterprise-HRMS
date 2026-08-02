d09ef317f945dcddfc01e826b6a4cf0829231e40
Pratik Beladiya <beladiyapratik03@gmail.com>
2026-08-02 17:41:35 +0530

    solve connnection between server and database

--- src/server/db/db.js
+++ src/server/db/db.js
@@ -1,15 +1,22 @@
- const mongoose = require("mongoose");
+const mongoose = require("mongoose");
+const dns = require("dns");
 
- async function ConnectToDb(){
-    await mongoose.connect(process.env.MONGO_URI)
+// Custom DNS servers to fix querySrv ECONNREFUSED issues on local Windows networks / ISPs when connecting to MongoDB Atlas +srv URIs
+try {
+  dns.setServers(["8.8.8.8", "1.1.1.1"]);
+} catch (e) {
+  // Ignore error if system environment restricts setting DNS
+}
 
-    .then (()=>{
-        console.log("database connected successfully");
-     }).catch (()=>{
-        console.log("database not connected");
-          
-         })
- }
+async function ConnectToDb() {
+  await mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+      console.log("database connected successfully");
+    })
+    .catch((err) => {
+      console.log("database not connected:", err.message);
+    });
+}
 
- module.exports=ConnectToDb;
-
+module.exports = ConnectToDb;
