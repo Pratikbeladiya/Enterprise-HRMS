e3b85f7f3c19a600934ab8afd6be4085bd13c78b
utsavbeladiya268-dot <utsavbeladiya268@gmail.com>
2026-08-02 17:20:40 +0530

    Configure admin authentication routes for signup and login

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,6 +5,8 @@ const router = express.Router();
 const {
   signup,
   login,
+   adminSignup,
+  adminLogin,
 } = require("../controllers/authController");
 
 // ==========================
@@ -16,5 +18,15 @@ router.post("/signup", signup);
 
 // User Login
 router.post("/login", login);
+
+// ==========================
+// Admin Authentication Routes
+// ==========================
+
+// Admin Signup
+router.post("/admin-signup", adminSignup);
+
+// Admin Login
+router.post("/admin-login", adminLogin);
 
 module.exports = router;
