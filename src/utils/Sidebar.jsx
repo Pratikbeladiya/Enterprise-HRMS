2336c7449ce8992dc40c872ca516006cb269a0d6
pratik_beladiya <beladiyapratik03@gmail.com>
2026-08-09 21:53:40 +0530

    Add Sidebar component for dashboard navigation

---
+++ src/utils/Sidebar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <aside className="sidebar">
+      <h3>Dashboard</h3>
+
+      <ul>
+        <li>Home</li>
+        <li>Employees</li>
+        <li>Attendance</li>
+        <li>Payroll</li>
+        <li>Leave Management</li>
+        <li>Settings</li>
+      </ul>
+    </aside>
+  );
+};
+
+export default Sidebar;
