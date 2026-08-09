dd4708ab95d3e2d0a2994e97beff6ce4081ca1e4
pratik_beladiya <beladiyapratik03@gmail.com>
2026-08-09 21:55:12 +0530

    Add Footer component for project branding

---
+++ src/utils/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div>
+        <h3>MyProject</h3>
+        <p>Smart workforce management solution.</p>
+      </div>
+
+      <div>
+        <p>© 2026 MyProject. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
