fbbb43fba69b097d7d951411e37dff8df7074e3e
Arka <arkamondal1298@gmail.com>
2026-08-10 19:50:58 +0530

    remove codes

--- HRMSfrontend/src/App.jsx
+++ HRMSfrontend/src/App.jsx
@@ -41,7 +41,7 @@ export default function App() {
   const [darkMode, setDarkMode] = useState(false);
   const [activeTab, setActiveTab] = useState("Dashboard");
   const [sidebarOpen, setSidebarOpen] = useState(false);
-  const [isCollapsed, setIsCollapsed] = useState(false);
+  // const [isCollapsed, setIsCollapsed] = useState(false);
 
   // these state hooks for QR--------
   const [selectedQrEmployee, setSelectedQrEmployee] = useState(null);
