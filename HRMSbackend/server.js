84de624667ec0c2294bbf6c965d57d5b080921cc
Arka <arkamondal1298@gmail.com>
2026-08-17 20:13:29 +0530

    add employee backend #3

--- HRMSbackend/server.js
+++ HRMSbackend/server.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 
 const authRoutes = require('./routes/authRoutes');
 const attendanceRoutes = require('./routes/attendanceRoutes');
+const employeeRoutes = require('./routes/employeeRoutes');
 
 const app = express();
 
@@ -15,6 +16,7 @@ app.use(express.json());
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/attendance', attendanceRoutes);
+app.use('/api/employees', employeeRoutes);
 
 // Health Check
 app.get('/api/health', (req, res) => {
